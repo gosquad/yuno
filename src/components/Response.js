687532a8a632b61f56567f05ff2215208d666d50
Raymond Wan <rayxwan@gmail.com>
2015-12-28 15:18:11 -0500

    :cherry_blossom: [Response] Add hr

--- src/components/Response.js
+++ src/components/Response.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default class Response extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Resulting Response</h2>
+      <div className='response'>
+        <hr/>
+        <h2>Response</h2>
         <pre>
           { JSON.stringify(this.props.response, null , 2) } 
         </pre>
